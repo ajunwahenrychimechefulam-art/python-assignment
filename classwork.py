2bdb592b4c2e51fb004627846759be3a6afe1a9a
Henry <ajunwahenrychimechefulam@gmail.com>
2025-11-11 15:32:54 -0800

    classwork

--- classwork.py
+++ classwork.py
@@ -64,3 +64,53 @@ age = 19
 school = "Bright Future Academy"
 
 print(f"My name is Rahl/nl'n and i am 19 years old, and i attend Bright Future Academy")
+
+
+txt = "The best in life are Free"
+if "expenisive" not in txt:
+    print("expensive not in txt")
+
+    b = "  Hello, world"
+    print(b[6])
+    print(name.upper())
+    print(name.replace("H", "j"))
+    print(name.split(","))
+
+    name ='Rahl'
+    career = 'software Developer'
+    nationality = 'Nigeria'
+    Bio = name + " " +  career  + " " + nationality
+    print(Bio)
+
+
+    price = 30
+    print(f"I need {price: .2f} money")
+    print(price)
+
+    txt = "we are the so-called \"Vikings\" from the north."
+    print(txt)
+    txt = "we are coming \'school' from school"
+
+    txt = "looking at them \\girls." 
+    print(txt)
+
+    txt = "good morning\nworld!"
+    print(txt)
+
+    txt = "hi\rWorld!"
+    print(txt)
+
+    txt = "mango\t world!"
+    print(txt)
+
+    txt = "promis\b to henry!"
+    print(txt)
+
+    txt = "trees\f i love!"
+    print(txt)
+
+    txt = "\mango\ tree\ world\ henry\ love\ "
+    print(txt)
+
+    txt = "\ xmango\ xtree\ xworld\ xhenry\ xlove\ "
+    print(txt)
